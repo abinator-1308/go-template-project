88e27379b9fd4fbd747da3dbd34993da3d3d8328
Harish Bohara <harish.bohara@gmail.com>
2022-01-30 23:58:30 +0530

    first cut with composit dynamo

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@ go mod tidy
 go generate ./...
 go fmt ./...
 
-export INCLUDE_DYNAMO_TESTS=true
-export INTEGRATION_TESTS=true
+OUTPUT=$(aws dynamodb list-tables  | grep users | wc -l | cut -c8-)
+if [ ${OUTPUT} == "1" ]; then
+  echo "DynamoDB connection is Ok - we can run integration tests"
+  export INCLUDE_DYNAMO_TESTS=true
+  export INTEGRATION_TESTS=true
+else
+    echo "****************** No DynamoDB connection - skip integration tests ******************"
+fi
+
 go  test ./... -v
